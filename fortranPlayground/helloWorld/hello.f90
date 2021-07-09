d06e056166a5477d5b943f2e8b8f038e3b4031ce
DavidVadnais <david.vadnais@protonmail.com>
2021-07-09 14:03:01 -0600

    extended hello world - fortran

--- fortranPlayground/helloWorld/hello.f90
+++ fortranPlayground/helloWorld/hello.f90
@@ -1,4 +1,21 @@
-program hello
-        implicit none
-        write(*,*) 'Hello world!'
-end program hello
+!https://www.fortrantutorial.com/loops/index.php
+
+Program Hello
+  implicit none                                        ! checks if you did good
+  !an example of program  structure                    !b: a  comment  
+  real :: answer,x,y                                   !c: declarations
+  integer  ::  i
+
+  print  *, 'Enter two numbers'                        !d: output
+  read  *, x                                           !e: input
+  read  *, y                                           !e: input
+  answer=x+y                                           !f: arithmetic
+  print  *, 'The total is ', answer                    !g: output
+  print *, "Hello World"
+  
+  !lOOPS
+  
+  do  i=0,20
+  	print  *,i
+  end  do
+End Program Hello
