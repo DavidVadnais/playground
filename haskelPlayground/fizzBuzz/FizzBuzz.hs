5d7c5830b2c140b057cb3cec6cad559a60f4d956
DavidVadnais <david.vadnais@protonmail.com>
2021-08-18 18:02:05 -0600

    haskell fizzbuzz

---
+++ haskelPlayground/fizzBuzz/FizzBuzz.hs
@@ -0,0 +1,12 @@
+module Main where
+
+-- based on https://github.com/zenware/FizzBuzz/blob/master/haskell.hs
+
+fizzbuzz :: Int -> String
+fizzbuzz x
+    | x `mod` 15 == 0 = "FizzBuzz"
+    | x `mod` 3  == 0 = "Fizz"
+    | x `mod` 5  == 0 = "Buzz"
+    | otherwise       = show x
+
+main = mapM (putStrLn . fizzbuzz) [1..100]
